1dff2ede0edc56920b0569bf254b957ad3a3a888
Timothy Munene <munenegicharu@gmail.com>
2020-08-31 08:44:54 +0300

    Add logic for pipe

--- src/app/reverse-string.pipe.ts
+++ src/app/reverse-string.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReverseStringPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    let newStr: string = "";
+    for (var i = value.length-1; i >= 0; i -- ) {
+    newStr += value.charAt(i);
+  }
+    return newStr;
   }
 
 }
